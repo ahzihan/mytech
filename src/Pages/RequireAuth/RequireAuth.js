fa1e782ff1961c96fe5631902482a8897dc326ba
Md Akbar Hossain <akbarhossain160@gmail.com>
2022-06-23 22:37:41 +0600

    send verification email is donne

--- src/Pages/RequireAuth/RequireAuth.js
+++ src/Pages/RequireAuth/RequireAuth.js
@@ -3,10 +3,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../firebase.init';
 import Loading from '../Share/Loading/Loading';
+import { useSendEmailVerification } from 'react-firebase-hooks/auth';
+import { ToastContainer,toast } from 'react-toastify';
 
 const RequireAuth = ( { children } ) => {
     const [ user, loading ] = useAuthState( auth );
     const location = useLocation();
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
 
     if ( loading ) {
         return <Loading></Loading>;
@@ -19,7 +22,11 @@ const RequireAuth = ( { children } ) => {
         return <div className='text-center'>
             <h3 className='text-danger'>Your email is not varified!!</h3>
             <h6>Please, Verify Your Email.</h6>
-            <button className='btn btn-primary mb-3'>Send Verify Code Again</button>
+            <button onClick={async () => {
+          await sendEmailVerification();
+          toast('Email sending, please check your mail.');
+        }} className='btn btn-primary mb-3'>Send Verify Code Again</button>
+        <ToastContainer />
         </div>;
     }
     return children;
